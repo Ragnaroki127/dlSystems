6791145616df00e57dac292a727a974c7b301cb5
Ragnaroki1127 <yanhaoxin1207@gmail.com>
2023-09-21 21:34:14 +0800

    0921 hw2 Q4 ongoing

--- hw2/python/needle/data.py
+++ hw2/python/needle/data.py
@@ -1,8 +1,46 @@
 import numpy as np
 from .autograd import Tensor
+import gzip
 
 from typing import Iterator, Optional, List, Sized, Union, Iterable, Any
 
+def parse_mnist(image_filename, label_filename):
+    """ Read an images and labels file in MNIST format.  See this page:
+    http://yann.lecun.com/exdb/mnist/ for a description of the file format.
+
+    Args:
+        image_filename (str): name of gzipped images file in MNIST format
+        label_filename (str): name of gzipped labels file in MNIST format
+
+    Returns:
+        Tuple (X,y):
+            X (numpy.ndarray[np.float32]): 2D numpy array containing the loaded 
+                data.  The dimensionality of the data should be 
+                (num_examples x input_dim) where 'input_dim' is the full 
+                dimension of the data, e.g., since MNIST images are 28x28, it 
+                will be 784.  Values should be of type np.float32, and the data 
+                should be normalized to have a minimum value of 0.0 and a 
+                maximum value of 1.0. The normalization should be applied uniformly
+                across the whole dataset, _not_ individual images.
+
+            y (numpy.ndarray[dtype=np.uint8]): 1D numpy array containing the
+                labels of the examples.  Values should be of type np.uint8 and
+                for MNIST will contain the values 0-9.
+    """
+    ### BEGIN YOUR CODE
+    with gzip.open(image_filename, 'rb') as f:
+        images = f.read()
+
+    X = np.frombuffer(images, dtype=np.uint8)[16:]
+    X = X.astype(np.float32).reshape((-1, 784))
+    X /= X.max()
+
+    with gzip.open(label_filename, 'rb') as f:
+        labels = f.read()
+
+    y = np.frombuffer(labels, dtype=np.uint8)[8:]
+    return X, y
+    ### END YOUR CODE
 
 class Transform:
     def __call__(self, x):
@@ -24,7 +62,10 @@ class RandomFlipHorizontal(Transform):
         """
         flip_img = np.random.rand() < self.p
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        if flip_img:
+            return np.flip(img, 1)
+        else:
+            return img
         ### END YOUR SOLUTION
 
 
@@ -42,7 +83,8 @@ class RandomCrop(Transform):
         """
         shift_x, shift_y = np.random.randint(low=-self.padding, high=self.padding+1, size=2)
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        padded_img = np.pad(img, ((self.padding, self.padding), (self.padding, self.padding), (0, 0)), mode='constant', constant_values=0.)
+        return padded_img[self.padding + shift_x: self.padding + img.shape[0] + shift_x, self.padding + shift_y: self.padding + img.shape[1] + shift_y]
         ### END YOUR SOLUTION
 
 
